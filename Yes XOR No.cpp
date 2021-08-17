77534925130cb68cda9a85c8254a3ff6bcbaef99
Vivek Kumar Srivastava <43187055+Viv786ek@users.noreply.github.com>
2021-08-17 12:37:02 +0530

    Create Yes XOR No.cpp
    https://practice.geeksforgeeks.org/problems/yes-xor-no2901/1

---
+++ Yes XOR No.cpp
@@ -0,0 +1,9 @@
+//https://practice.geeksforgeeks.org/problems/yes-xor-no2901/1
+
+class Solution {
+  public:
+    string yesXorNo(int N, long long A[], long long B[]) {
+        // code here
+        return "Yes";
+    }
+};
